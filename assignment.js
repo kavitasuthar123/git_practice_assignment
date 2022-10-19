6fb666ba3b3f77a2bc1ea11422e4789ccb9297c3
kavitasuthar123 <kavitasuthar.in@gmail.com>
2022-10-19 19:39:58 +0530

    code added

---
+++ assignment.js
@@ -0,0 +1,19 @@
+let num=15;
+let count="";
+for (let i=1;i<=num;i++){
+     if(num%i=0)
+   {count++;}}
+if(count=2){
+console.log("Yes")}
+else{
+console.log("No")}
+
+
+
+//check whether a string is palindrome or not//
+let name="nitin";
+let bag="";
+for (let i=name;i>=0;--i){
+      if(name==bag){
+console.log("yes")}
+else{console.log("No")}}
